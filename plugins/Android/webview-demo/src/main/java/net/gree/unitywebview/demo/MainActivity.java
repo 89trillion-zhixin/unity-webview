7c228aea2759ec97c2c0852494fb36c921fcc535
89trillion-zhixin <linzhixin@89trillion.com>
2022-11-01 17:53:21 +0800

    [feat]增加webview预加载Demo

---
+++ plugins/Android/webview-demo/src/main/java/net/gree/unitywebview/demo/MainActivity.java
@@ -0,0 +1,78 @@
+package net.gree.unitywebview.demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import net.gree.unitywebview.Utils;
+import net.gree.unitywebview.loader.BrowserLoader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url = "http://test.act.test.aow.com:1005/beginnerHeroPackage1/1?price=%24%201.99&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjI3NzIxNjYsImlzcyI6Imdpbi1ibG9nIn0.zlffvOaEkXb7lTFmKCweKsCVURBdwYOkIqBVvphCCrg&basicInfo=eyJwZiI6ImFuZHJvaWQiLCJzdmMiOjI1LCJjdmMiOjQxNDAsImRldmljZSI6IlNNLU45NTAwIiwidWlkIjoiZmZmZmZmZmYtZWJiNS05MTVmLTAwMDAtMTkxMjEyMTIxNSIsImNoYW4iOiIxMDAwMSIsImFkaWQiOiIiLCJnYWlkIjoiZmZmZmZmZmYtZWJiNS05MTVmLTAwMDAtMTkxMjEyMTIxNSIsIm5ldHdvcmsiOiJ3aWZpIiwibGFuZyI6ImVuX1VTIn0%3D&lang=en";
+    private FrameLayout webviewContainer;
+    private WebView webView;
+    private WebView tempWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webviewContainer = findViewById(R.id.webview_container);
+        webView = findViewById(R.id.webview);
+
+        Button preloadBtn = findViewById(R.id.preload_button);
+        preloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "开始预加载", Toast.LENGTH_SHORT).show();
+
+                BrowserLoader.getInstance(getApplicationContext()).preloadUrl(url, false, 1, "");
+            }
+        });
+
+        Button openWebViewBtn = findViewById(R.id.open_webview);
+        openWebViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Utils.configWebViewDefaults(webView, false, 1, "");
+                webView.loadUrl(url);
+            }
+        });
+
+        Button createWebViewBtn = findViewById(R.id.create_webview);
+        createWebViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tempWebView = new WebView(getApplicationContext());
+                Utils.configWebViewDefaults(tempWebView, false, 1, "");
+                tempWebView.loadUrl(url);
+                webviewContainer.addView(tempWebView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            }
+        });
+
+        Button closeWebViewBtn = findViewById(R.id.close_webview);
+        closeWebViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tempWebView != null) {
+                    tempWebView.stopLoading();
+                    tempWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+                    webviewContainer.removeView(tempWebView);
+                    tempWebView.destroy();
+                    tempWebView = null;
+                }
+
+                webView.stopLoading();
+                webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            }
+        });
+    }
+}
